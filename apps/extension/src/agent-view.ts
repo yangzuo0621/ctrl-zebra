0cac970aa4e5fbf631ff10cdc4a588afb69c1d5f
Zuo Yang <zyang0508@163.com>
2026-07-15 13:32:45 +0800

    feat(T0104): configure Webview security policy

--- apps/extension/src/agent-view.ts
+++ apps/extension/src/agent-view.ts
@@ -1,7 +1,10 @@
+import { randomBytes } from "node:crypto";
+
 import {
   type Disposable,
   Uri,
   type Webview,
+  type WebviewOptions,
   type WebviewView,
   type WebviewViewProvider,
 } from "vscode";
@@ -10,7 +13,7 @@ export const agentViewId = "ctrlZebra.agentView";
 
 type WebviewViewRegistrar = (viewId: string, provider: WebviewViewProvider) => Disposable;
 
-type WebviewResourceResolver = Pick<Webview, "asWebviewUri">;
+type WebviewResourceResolver = Pick<Webview, "asWebviewUri" | "cspSource">;
 
 function escapeHtmlAttribute(value: string): string {
   return value
@@ -21,21 +24,45 @@ function escapeHtmlAttribute(value: string): string {
     .replaceAll(">", "&gt;");
 }
 
-export function createAgentViewHtml(webview: WebviewResourceResolver, extensionUri: Uri): string {
-  const scriptUri = webview.asWebviewUri(Uri.joinPath(extensionUri, "dist", "webview", "main.js"));
-  const styleUri = webview.asWebviewUri(Uri.joinPath(extensionUri, "dist", "webview", "main.css"));
+function getWebviewResourceRoot(extensionUri: Uri): Uri {
+  return Uri.joinPath(extensionUri, "dist", "webview");
+}
+
+export function createAgentViewOptions(extensionUri: Uri): WebviewOptions {
+  return {
+    enableScripts: true,
+    localResourceRoots: [getWebviewResourceRoot(extensionUri)],
+  };
+}
+
+export function createAgentViewHtml(
+  webview: WebviewResourceResolver,
+  extensionUri: Uri,
+  nonce: string,
+): string {
+  const resourceRoot = getWebviewResourceRoot(extensionUri);
+  const scriptUri = webview.asWebviewUri(Uri.joinPath(resourceRoot, "main.js"));
+  const styleUri = webview.asWebviewUri(Uri.joinPath(resourceRoot, "main.css"));
+  const contentSecurityPolicy = [
+    "default-src 'none'",
+    `style-src ${webview.cspSource}`,
+    `script-src 'nonce-${nonce}'`,
+    "base-uri 'none'",
+    "form-action 'none'",
+  ].join("; ");
 
   return `<!doctype html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="Content-Security-Policy" content="${escapeHtmlAttribute(contentSecurityPolicy)}" />
     <link rel="stylesheet" href="${escapeHtmlAttribute(styleUri.toString())}" />
     <title>CtrlZebra</title>
   </head>
   <body>
     <div id="root"></div>
-    <script type="module" src="${escapeHtmlAttribute(scriptUri.toString())}"></script>
+    <script type="module" nonce="${escapeHtmlAttribute(nonce)}" src="${escapeHtmlAttribute(scriptUri.toString())}"></script>
   </body>
 </html>`;
 }
@@ -44,8 +71,9 @@ class AgentViewProvider implements WebviewViewProvider {
   constructor(private readonly extensionUri: Uri) {}
 
   resolveWebviewView(webviewView: WebviewView): void {
-    webviewView.webview.options = { enableScripts: true };
-    webviewView.webview.html = createAgentViewHtml(webviewView.webview, this.extensionUri);
+    webviewView.webview.options = createAgentViewOptions(this.extensionUri);
+    const nonce = randomBytes(16).toString("hex");
+    webviewView.webview.html = createAgentViewHtml(webviewView.webview, this.extensionUri, nonce);
   }
 }
 
